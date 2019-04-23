8ab6020878b7d987307697e25aa51597982f2687
Jeremy-NZ <jeremydrummond11@gmail.com>
2019-04-23 15:21:29 +1200

    add actions to retrieve transaction meta data

--- client/src/app/transactions/state/transaction.actions.ts
+++ client/src/app/transactions/state/transaction.actions.ts
@@ -1,10 +1,14 @@
 import { Action } from '@ngrx/store';
 import { Transaction } from '../transaction';
+import { TransactionMetaData } from '../TransactionMetaData';
 
 export enum TransactionActionTypes {
     GetRecentTransactions = '[Transaction] Get Recent Transactions',
     GetTransactionsSuccess = '[Transaction] Get Transactions Success',
     GetTransactionsFailure = '[Transaction] Get Transactions Failure',
+    GetTransactionMetaData = '[Transaction] Get Transaction Meta Data',
+    GetTransactionMetaDataSuccess = '[Transaction] Get Transaction Meta Data Success',
+    GetTransactionMetaDataFailure = '[Transaction] Get Transaction Meta Data Failure',
     InitializeCurrentTransaction = '[Transaction] Initialize Current Transaction',
     CreateTransaction = '[Transaction] Create Transaction',
     CreateTransactionFailure = '[Transaction] Create Transaction Failure',
@@ -23,6 +27,22 @@ export class GetTransactionsSuccess implements Action {
 
 export class GetTransactionsFailure implements Action {
     readonly type = TransactionActionTypes.GetTransactionsFailure;
+
+    constructor(public errorMessage: string) { }
+}
+
+export class GetTransactionMetaData implements Action {
+    readonly type = TransactionActionTypes.GetTransactionMetaData;
+}
+
+export class GetTransactionMetaDataSuccess implements Action {
+    readonly type = TransactionActionTypes.GetTransactionMetaDataSuccess;
+
+    constructor(public transactionMetaData: TransactionMetaData) { }
+}
+
+export class GetTransactionMetaDataFailure implements Action {
+    readonly type = TransactionActionTypes.GetTransactionMetaDataFailure;
 
     constructor(public errorMessage: string) { }
 }
@@ -50,10 +70,14 @@ export class CreateTransactionSuccess implements Action {
  * Union the action classes into a single union type which represents all the action creators.
  * This allows us to strongly type the reducer
  * */
-export type TransactionActions = GetRecentTransactions
-    | GetTransactionsSuccess
-    | GetTransactionsFailure
-    | InitializeCurrentTransaction
+export type TransactionActions = InitializeCurrentTransaction
     | CreateTransaction
     | CreateTransactionFailure
-    | CreateTransactionSuccess;
+    | CreateTransactionSuccess
+    | GetTransactionMetaData
+    | GetTransactionMetaDataFailure
+    | GetTransactionMetaDataSuccess
+    | GetRecentTransactions
+    | GetTransactionsSuccess
+    | GetTransactionsFailure;
+
